30e7349bfac822ffa281caa10c1f760ed00eb4b4
System <none>
2017-04-25 02:07:49 +0000

    Server is working

--- board.js
+++ board.js
@@ -3,6 +3,7 @@
 const _ = require('lodash')
 const util = require('util')
 
+
 const DIRECTION = {
   north: 0,
   east: 1,
@@ -21,11 +22,30 @@ class Board {
   constructor(options) {
     this.players = []
     this.size = 25
-    this.winner = 3
+//    this.winner = 3
+    this.open = false
     _.assign(this, options)
     return this
   }
   
+  channel(){
+    return 'board-' + this.id
+  }
+
+  broadcast(event,data){
+    this.server.to(this.channel()).emit(event,data)
+  }
+
+  update(player){
+    this.broadcast('update',player)
+  }
+  
+  refresh() {
+    var me = this
+    //console.log(me.players)
+    _.forEach(me.players, function(p) { me.update(p) })
+  }
+
   hasPlayer(player){
     return _.find(this.players, function(p) { return p.id === player.id })
   }
@@ -40,66 +60,76 @@ class Board {
     player.direction = _.random(DIRECTION.north,DIRECTION.west)
     player.directionStr = CARDINAL[player.direction]
     do {
-      player.x = _.random(0, this.size)
-      player.y = _.random(0, this.size)
+      player.x = _.random(0, this.size-1)
+      player.y = _.random(0, this.size-1)
     } while(this.isCellOccupied(player.x,player.y))
-  
+    player.channel = this.channel()
     this.players.push(player)
-    
-    this.broadcast('joined',player)
-    console.log(util.format('[%s] has joined at (%d,%d) facing %s', player.name, player.x, player.y, player.directionStr))
+    this.refresh()
+    return player
+  }
+  
+  removePlayer(player){
+    if(this.hasPlayer(player)){
+      _.remove(this.players,(p) => { return p.id === player.id })
+      delete player.channel
+      this.broadcast('remove',player)
+    }
   }
                    
   isCellOccupied(x,y){
-    return _.find(this.players,function(p) { p.x === x && p.y === y })
-  }
-  
-  broadcast(type,message){
-    _.forEach(this.players,function(p) { p.socket.emit(type,message) })
+    return _.find(this.players,function(p) { return p.x === x && p.y === y })
   }
   
   turnLeft(player){
+    //console.log('start: x %d y %d d %s', player.x, player.y, player.directionStr)
     player.direction -= 1
     if(player.direction < DIRECTION.north) {
       player.direction = DIRECTION.west
     }
     player.directionStr = CARDINAL[player.direction]
     this.update(player)
+    //console.log('  end: x %d y %d d %s', player.x, player.y, player.directionStr)
     return true
   }
   
   turnRight(player){
+    //console.log('start: x %d y %d d %s', player.x, player.y, player.directionStr)
     player.direction += 1
     if(player.direction > DIRECTION.west) {
       player.direction = DIRECTION.north
     }
     player.directionStr = CARDINAL[player.direction]
     this.update(player)
+    //console.log('  end: x %d y %d d %s', player.x, player.y, player.directionStr)
     return true
   }
   
   moveForward(player){
     var x = player.x
     var y = player.y
+    //console.log('start: x %d y %d d %s', player.x, player.y, player.directionStr)
     switch(player.direction){
       case DIRECTION.north:
-        x += 1
-        break
-      case DIRECTION.east:
         y += 1
         break
+      case DIRECTION.east:
+        x += 1
+        break
       case DIRECTION.south:
-        x -= 1
+        y -= 1
         break
       case DIRECTION.west:
-        y -= 1
+        x -= 1
         break;
     }
     if(x<0 || x >= this.size || y < 0 || y >= this.size || this.isCellOccupied(x,y)){
+      //console.log('rejected')
       return false
     }
     player.x = x
     player.y = y
+    //console.log('  end: x %d y %d d %s', player.x, player.y, player.directionStr)    
     this.update(player)
     return true
   }
@@ -109,39 +139,148 @@ class Board {
     var y = player.y
     switch(player.direction){
       case DIRECTION.north:
-        x += 1
-        break
-      case DIRECTION.east:
         y += 1
         break
+      case DIRECTION.east:
+        x += 1
+        break
       case DIRECTION.south:
-        x -= 1
+        y -= 1
         break
       case DIRECTION.west:
-        y -= 1
+        x -= 1
         break;
     }
+    //console.log('  tag: x %d y %d d %s', x, y, player.directionStr)
     var target = this.isCellOccupied(x,y)
     if(target){
       target.tagged += 1
       player.score += 1
-      this.broadcast('update',target)
-      this.broadcast('update',player)
-      console.log(util.format('[%s] was tagged by [%s]!', target.name, player.name))
+      //console.log(util.format('[%s] was tagged by [%s]!', target.name, player.name))
+      this.update(target)
+      this.update(player)
+      this.broadcast('status',util.format('[%s] was tagged by [%s]!', target.name, player.name))
       /*
       if(player.score >= this.winner){
         this.broadcast('winner',player)
         console.log(util.format('[%s] has won!', player.name))
       }
       */
+      return true
     }
+    return false
   }
   
-  update(player){
-    this.broadcast('update',player)
-    console.log(util.format('[%s] is now at (%d,%d) facing %s', player.name, player.x, player.y, player.directionStr))    
+  // returns 0 if there are no bots in a straight line in front of where bot is facing
+  // otherwise, returns number of 'moveForward' commands to bring bot to next bot
+  /*
+  distanceToBot(player){
+  }
+  */
+  
+  // returns number of 'moveForward' commands to next wall
+  distanceToWall(player){
+    var result = -1
+    switch(player.direction){
+      case DIRECTION.north:
+        result = this.size - 1 - player.y;
+        break;
+      case DIRECTION.east:
+        result = this.size - 1 - player.x;
+        break;
+      case DIRECTION.south:
+        result = player.y;
+        break;
+      case DIRECTION.west:
+        result = player.x;
+        break;
+    }
+    console.log('Distance to wall for (%d,%d) is %d', player.x, player.y, result)
+    return result
   }
   
+  run(){
+    this.loop = true
+    this.done = false
+    this._run()
+    var timer
+    timer = setInterval(() => {
+      if(!this.loop) {
+        clearInterval(timer)
+      } else if(this.done === true) { 
+        this.done = false
+        this._run()
+      }
+    }, 100)
+  }
+    
+  _run(){
+    var me = this
+    var commands = []
+      
+    var sockets_in_room = me.server.nsps['/'].adapter.rooms[me.channel()]
+    var promises = []
+    var status = {}
+    for (var socketId in sockets_in_room.sockets) {
+      var client = me.server.sockets.connected[socketId]
+      if(client.hasOwnProperty('player')){
+        //console.log('request command from [%s] at %s',client.player.name,socketId)
+        status[client.id] = undefined
+        var promise = new Promise((resolve,reject) => {
+          client.emit('request command',me.channel(),(command) => {
+            commands.push({player: client.player, command: command, client: client})
+            status[client.id].status = true
+            resolve(1)
+          })
+          setTimeout(() => {
+            status[client.id].status = false
+            reject(client.player) 
+          },500)
+        })
+        promises.push(promise)
+        status[client.id] = {status: undefined, promise: promise}
+      }        
+    }
+    Promise.all(promises).then(() => {
+      var responses = 0
+      _.forEach(commands,function(cmd){
+        var result = false
+        responses += 1
+        switch(cmd.command){
+          case 'turn left':
+            result = me.turnLeft(cmd.player)
+            break;
+          case 'turn right':
+            result = me.turnRight(cmd.player)
+            break;
+          case 'move forward':
+            result = me.moveForward(cmd.player)
+            break;
+          case 'tag':
+            result = me.tag(cmd.player)
+            break;
+          case 'distance to wall':
+            result = me.distanceToWall(cmd.player)
+            break;
+          default:
+            console.log('Unrecognized command \'%s\' from [%s]', cmd.command, cmd.player.name)
+        }
+        //console.log('responses %d of %d', responses, promises.length)
+        if(responses === promises.length){
+          me.done = true
+        }
+        cmd.client.emit('result',{command: cmd.command, result: result})        
+      })
+    })
+    .catch((player) => {
+      console.log('Timeout waiting for [%s] to respond', player.name)
+      me.done = true
+    })
+  }
+
+  stop(){
+    this.loop = false
+  }
 }
 
 module.exports = Board
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = require('express')()
 const http = require('http').Server(app)
 const server = require('socket.io')(http)
 const _ = require('lodash')
+
 const Player = require('./player')
+const Board = require('./board')
 
 // pull in system config
 const port = process.env.PORT || 3000
@@ -28,33 +30,84 @@ app.get('/', function (req,res){
 
 // server-wide globals
 let clientId = 0   // unique client id
-let boards = []    // list of playing boards
 let players = []   // list of players
+let boardId = 0    // unique board id
+let boards = []    // list of active boards
 
 // accept new connections from the client
-server.on('connection', function (client){
+var main = server.of('/')
+
+main.on('connection', function (client){
   // client-wide globals
   let player
-  let id
-  
-  // accept the 'hello, my name is...' message from the client
+  let playerId
+    
+  // player has connected and is ready to start editing
   client.on('auth', (opts, cb) => {  // 'opts' contains the name data from the client, 'cb' is how we send a message back to the client
-    id = ++clientId
+    playerId = ++clientId
     player = new Player(_.assign({
-      id: id,
-      socket: client,
-      name: 'Player ' + id
+      id: playerId,
+      name: 'Player ' + playerId,
+      score: 0,
+      tagged: 0,
     }, opts))
     players.push(player)
+    
     client.player = player
+
     console.log('[%s] has joined', player.name)
-    // a player will start with a test board
+    server.emit('status', '[' + player.id + '] <font color="red">' + player.name + '</font> has joined.')
+    
+    cb(player)
   })
   
-  // accept the disconnect message
+  // player is in editing and wants to test their robot; put them in a 'private' board and start it running
+  client.on('test', () => {
+    var board = new Board({id: ++boardId, server: server})
+    client.join(board.channel())
+    board.addPlayer(player)
+    boards.push(board)
+    client.board = board
+    board.run()
+  })
+  
+  // player is in test and wants to go back to editing
+  client.on('stop', () => {
+    var board = client.board
+    board.removePlayer(player)
+    client.leave(board.channel())
+    if(board.numPlayers() === 0){
+      board.stop()
+      _.remove(boards,function(b){return b.id === board.id})
+    }
+    delete client.board
+  })
+  
+  // player is in editing and wants to go to the arena (compete against other robots)
+  client.on('play', () => {
+    // if no open board, create a new one
+    // or, add player to existing open board
+  })
+  
+  // player is in the arena and wants to go back to editing
+  client.on('leave', () => {
+    
+  })  
+  
+  // player has left entirely
   client.on('disconnect', () => {
-    console.log('[%s] has left', client.player.name)
-    _.remove(players,client.player)
+    _.remove(players,function (p) {
+      return p.id === player.id
+    })
+    console.log('[%s] has left', player.name)
+    server.emit('status', '[' + player.id + '] <font color="red">' + player.name + '</font> has left.')
+    if(client.hasOwnProperty('board')){
+      var board = client.board
+      board.removePlayer(player)
+      if(board.numPlayers() === 0){
+        _.remove(function(b){return b.id === board.id})
+      }
+    }
   })
 })
 
